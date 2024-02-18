518b22c1ceb62eebf84bb2518b8bdae21d48a4ee
Fuad Hasan <fhrabbi555@gmail.com>
2024-02-18 22:54:08 +0600

    Adding BMI Calculation Methods

--- lib/main.dart
+++ lib/main.dart
@@ -13,25 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'BMI-Calculators'),
+      home: const MyHomePage(title: 'BMI-Calculator'),
     );
   }
 }
@@ -39,14 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,25 +32,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var wtController = TextEditingController();
+  var ftController = TextEditingController();
+  var inController = TextEditingController();
+
+  var result = "";
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Text('Peace'),
+
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('BMI',style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),),
+
+              TextField(
+                controller: wtController,
+                decoration: InputDecoration(
+                  label: Text('Enter your Weight'),
+                  prefixIcon: Icon(Icons.line_weight)
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 30,),
+              TextField(
+                controller: ftController,
+                decoration: InputDecoration(
+                    label: Text('Enter your Height'),
+                    prefixIcon: Icon(Icons.height)
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 30,),
+              TextField(
+                controller: inController,
+                decoration: InputDecoration(
+                    label: Text('Enter your Age'),
+                    prefixIcon: Icon(Icons.numbers)
+                ),
+                keyboardType: TextInputType.number,
+              ),
+
+              SizedBox(height: 30,),
+
+              ElevatedButton(onPressed: (){
+
+                var wt = wtController.text.toString();
+                var ft = ftController.text.toString();
+                var inc = inController.text.toString();
+
+                if(wt != "" && ft != "" && inc != ""){
+
+                  var iwt = int.parse(wt);
+                  var ifit = int.parse(ft);
+                  var iinc = int.parse(inc);
+
+              //  Convert height to inches
+                  var heightInInches = (ifit * 12) + iinc;
+
+               //Calculate BMI
+                  var bmi = (iwt / (heightInInches * heightInInches)) * 703;
+
+                  setState(() {
+                    result = "Your BMI is: ${bmi.toStringAsFixed(4)}";
+                  });
+
+                }else{
+                  setState(() {
+                    result = "Please fill all the required blanks";
+                  });
+                }
+
+              }, child: Text('Calculate')),
+              SizedBox(height: 30,),
+              Text(result,style: TextStyle(fontSize: 15),)
+            ],
+          ),
+        ),
+      )
     );
   }
 }
